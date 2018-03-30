a8e8a28a1338b379a76f8943524439ac1c6d49a3
Vijaya Gowri <Vijaya Gowri>
2018-03-30 15:38:11 +0530

    done with routing

---
+++ src/app/app.router.ts
@@ -0,0 +1,21 @@
+import { NgModule} from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PnrStatusComponent } from './pnr-status/pnr-status.component';
+import { TrainBtwStationsComponent } from './train-btw-stations/train-btw-stations.component';
+import { LivetrainStatusComponent } from './livetrain-status/livetrain-status.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+
+const routes = [
+    {path:'pnr-status', component:PnrStatusComponent},
+    {path:'train-btw-stations', component:TrainBtwStationsComponent},
+    {path: 'livetrain-status', component:LivetrainStatusComponent},
+    {path:'contact-us', component:ContactUsComponent},
+    {path:'', redirectTo:'pnr-status', pathMatch:'full'}
+]
+
+@NgModule({
+    exports: [ RouterModule ],
+  imports: [ RouterModule.forRoot(routes) ],
+})
+
+export class AppRoutes {}
---
+++ src/app/livetrain-status/livetrain-status.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-livetrain-status',
+  templateUrl: './livetrain-status.component.html',
+  styleUrls: ['./livetrain-status.component.css']
+})
+export class LivetrainStatusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
