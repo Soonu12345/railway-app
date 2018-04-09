e55b14ca11f6c2f43b34de82f8efdf4f9203fa75
Your Namekalyani-venna <kalyani.veena@mtuity.com>
2018-04-09 15:35:37 +0530

    live status

--- src/app/livetrain-status/livetrain-status.component.ts
+++ src/app/livetrain-status/livetrain-status.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {LivetrainStatusService} from './livetrain-status.service'
 
 @Component({
   selector: 'app-livetrain-status',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LivetrainStatusComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private LivetrainStatusService:LivetrainStatusService) { }
+  trainNumber: any;
+  responseObj:object;
+  Date:string;
   ngOnInit() {
   }
+  getLiveTrainStatus() {
+    if (this.trainNumber) {
+      this.Date = '09-04-2018';
 
+      this.LivetrainStatusService.getLiveTrainStatus(this.trainNumber,this.Date).
+      subscribe(
+        responseObj => (this.responseObj = responseObj,
+          console.log(this.responseObj, 'success')),
+  
+        errormsg => (
+          console.log(errormsg.error.error, 'error')
+        )
+      )
+    }
+  }
 }
--- src/app/livetrain-status/livetrain-status.service.ts
+++ src/app/livetrain-status/livetrain-status.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import * as globals from '../globals';
 
 @Injectable()
 export class LivetrainStatusService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  reqUrl:string;
 
+  getLiveTrainStatus(trainNumber,Date): Observable<object> {
+    this.reqUrl=globals.base_url+'/v2/live/train/'+trainNumber+'/date/'+Date+'/'+'apikey/'+globals.apiKey+'/'
+   return this.http.get(this.reqUrl, trainNumber)
+  }
 }
